6f4e13b90e77221f704e61c40fbb4adc4d5d8230
Javier-Vincenti <364335@via.dk>
2026-05-13 09:47:44 +0200

    added edit profile feature

--- SEP_2/src/Main.java
+++ SEP_2/src/Main.java
@@ -8,9 +8,9 @@ import java.sql.SQLException;
 public class Main
 {
 
-  private static final String URL = "jdbc:postgresql://localhost:5432/internship";
+  private static final String URL = "jdbc:postgresql://localhost:5432/sep2";
   private static final String USER = "postgres";
-  private static final String PASSWORD = "admin";
+  private static final String PASSWORD = "Salta1981";
 
   public static Connection getConnection() throws SQLException {
     return DriverManager.getConnection(URL, USER, PASSWORD);
